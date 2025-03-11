05a4c0581d7cfb545846625f56ca32c541eb37c4
madinakhatukhova <Madina-Khat@yandex.ru>
2025-03-12 00:19:27 +0300

    1_SQL

--- src/main/java/pro/sky/web_demo/repository/StudentRepository.java
+++ src/main/java/pro/sky/web_demo/repository/StudentRepository.java
@@ -2,6 +2,12 @@ package pro.sky.web_demo.repository;
 
 import pro.sky.web_demo.model.Student;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> findByAgBetween(Integer min, Integer max);
+
+
 
 }
