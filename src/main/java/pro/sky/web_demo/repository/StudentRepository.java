b60bd2e38677045a29fe2853ceff91122e726fa7
madinakhatukhova <Madina-Khat@yandex.ru>
2025-03-20 21:01:15 +0300

    web-миграции и индексы

--- src/main/java/pro/sky/web_demo/repository/StudentRepository.java
+++ src/main/java/pro/sky/web_demo/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package pro.sky.web_demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pro.sky.web_demo.model.Student;
 
 import java.util.Collection;
@@ -10,6 +11,7 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     List<Student> findByAgeBetween(Integer min, Integer max);
 
-
+    @Query(value = "SELECT * FROM student ORDER BY id DESC LIMIT 5", nativeQuery = true)
+    Collection<Student> findFiveLast();
 }
 
---
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -0,0 +1,4 @@
+CREATE INDEX name_idx
+ ON student (name);
+ CREATE INDEX name_and_color_idx
+  ON faculty (name, color);
