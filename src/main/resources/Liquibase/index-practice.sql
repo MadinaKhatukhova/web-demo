1df37a0e7f6b47a40950ae6e784e6ac89584f68b
madinakhatukhova <Madina-Khat@yandex.ru>
2025-03-27 19:08:53 +0300

    web-миграции и индексы

---
+++ src/main/resources/Liquibase/index-practice.sql
@@ -0,0 +1,2 @@
+CREATE INDEX name_index ON student (name);
+CREATE INDEX name_and_color_index ON faculty (name, color);
--- src/main/resources/liquibase/scripts/index-practice.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX name_idx
- ON student (name);
- CREATE INDEX name_and_color_idx
-  ON faculty (name, color);
